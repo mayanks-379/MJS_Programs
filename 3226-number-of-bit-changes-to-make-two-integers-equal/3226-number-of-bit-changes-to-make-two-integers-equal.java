531ec93948e4b72d949b76e6b9ac0c3ee336bee0
mayanksingh-379 <52119501+mayanksingh-379@users.noreply.github.com>
2024-07-21 17:09:07 +0530

    Time: 0 ms (100.00%), Space: 40.7 MB (100.00%) - LeetHub

---
+++ 3226-number-of-bit-changes-to-make-two-integers-equal/3226-number-of-bit-changes-to-make-two-integers-equal.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int minChanges(int n, int k) {
+        if((n&k) != k)
+            return -1;
+        
+        if(n==k)
+            return 0;
+        
+        
+        int cnt=0;
+        while(n>0 || k>0){
+            int bitN = n&1;
+            int bitK = k&1;
+            
+            if(bitN==1 && bitK==0)
+                cnt++;
+            
+            n >>= 1;
+            k >>= 1;
+        }
+        
+        return cnt;
+
+    }
+}
