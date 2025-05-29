36f7921b47a92ade2723871ef4c8b3ac344c5d7a
mayanks-379 <52119501+mayanks-379@users.noreply.github.com>
2025-05-29 07:43:08 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (61.95%) - LeetSync

--- 3778-transform-array-by-parity/transform-array-by-parity.cpp
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -3,12 +3,7 @@ public:
     vector<int> transformArray(vector<int>& nums) {
         int n=nums.size();
         for(int i=0; i<nums.size(); i++){
-            if (nums[i]%2){
-                nums[i] = 1;
-            }
-            else{
-                nums[i] = 0;
-            }
+            nums[i] = nums[i]&1;
         }
         sort(nums.begin(), nums.end());
         return nums;
